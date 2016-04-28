18c9b5113da70b688f84fb7e531cb2e23066eb47
evaMwangi <mwangi.evalyne@students.jkuat.ac.ke>
2016-04-27 18:22:39 -0700

    importing vs inheritance

---
+++ day_3/export.py
@@ -0,0 +1,34 @@
+people = [
+
+	('joe', 78),
+	('janet', 90),
+	('Brian', 67)
+]
+
+
+def super_sum(*args):
+	return sum(args)
+
+
+def hello_again(name,age):
+	return "i am {} and {} years old".format(name,age)
+
+def max_min(A):
+	"""
+	takes the maximum and substract the minimum
+
+	"""
+	#return (max(A)-min(A)) its built in
+
+	max_, min_ = A[0], A[0]
+
+	for i in A:
+		if i > max_:
+			max_ = i
+		if i < min_:
+			min_ = i
+
+	return max_ - min_
+
+
+print max_min([1,4,7,5,4,9])
---
+++ day_3/importer.py
@@ -0,0 +1,7 @@
+from export import super_sum, hello_again, max_min, people
+
+a = [10,40,-5,6,9,34,7]
+print hello_again('joe', 89)
+print hello_again(*people[0])
+print super_sum(*a)
+print max_min(a)
